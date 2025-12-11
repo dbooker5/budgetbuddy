365bfd4442c4aed3d20387f0023c07f887cf9804
dbooker5 <bookerotieno21@gmail.com>
2025-12-11 17:41:29 +0300

    feat: Add home page
    - Create a new stateful widget for the home page.
    - Add a "Welcome Back" text message.
    - Use a column and row layout structure.

---
+++ lib/pages/home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      "Welcome Back",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
