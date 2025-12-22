5ef3dcb6d2b01fb8e699212e5556003df60e5b07
dbooker5 <bookerotieno21@gmail.com>
2025-12-22 14:21:19 +0300

    feat: Implement sign-up form UI
    - Replace `Column` with `Stack` to overlay the form on a background image.
    - Add a "Create Account!" title with specific text styling.
    - Implement text fields for "Name," "Email," and "Password," each with a corresponding label.
    - Style the input fields with white backgrounds, rounded corners, and prefix icons.

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -12,13 +12,116 @@ class _SignupState extends State<Signup> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Column(
+        child: Stack(
           children: [
             Image.asset(
               "images/signup.png",
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
               fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 50.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Create\nAccount!",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 100.0),
+                  Text(
+                    "Name",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Container(
+                    margin: EdgeInsets.only(right: 30.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.person,
+                          size: 28,
+                          color: Color(0xff904c6e),
+                        ),
+                        hintText: "Enter Name",
+                        hintStyle: TextStyle(fontSize: 18.0),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(
+                    "Email",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Container(
+                    margin: EdgeInsets.only(right: 30.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.mail,
+                          size: 28,
+                          color: Color(0xff904c6e),
+                        ),
+                        hintText: "Enter Email",
+                        hintStyle: TextStyle(fontSize: 18.0),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(
+                    "Password",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Container(
+                    margin: EdgeInsets.only(right: 30.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.password,
+                          size: 28,
+                          color: Color(0xff904c6e),
+                        ),
+                        hintText: "Enter Password",
+                        hintStyle: TextStyle(fontSize: 18.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
