d7848e6ad7d6b4db01958f2d079e727297b5d5d3
dbooker5 <bookerotieno21@gmail.com>
2025-12-23 16:12:51 +0300

    feat: Add navigation elements and scrollability to signup page
    - Wrap the form content in a `SingleChildScrollView` to prevent overflow on smaller screen sizes.
    - Add a "Next" button with a forward arrow icon.
    - Implement a "Login" text button for users who already have an account.

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -22,105 +22,159 @@ class _SignupState extends State<Signup> {
             ),
             Padding(
               padding: const EdgeInsets.only(left: 20.0, right: 50.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Create\nAccount!",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 35.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  SizedBox(height: 100.0),
-                  Text(
-                    "Name",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  SizedBox(height: 10.0),
-                  Container(
-                    margin: EdgeInsets.only(right: 30.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        prefixIcon: Icon(
-                          Icons.person,
-                          size: 28,
-                          color: Color(0xff904c6e),
-                        ),
-                        hintText: "Enter Name",
-                        hintStyle: TextStyle(fontSize: 18.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Create\nAccount!",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
-                  ),
-                  SizedBox(height: 20.0),
-                  Text(
-                    "Email",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  SizedBox(height: 10.0),
-                  Container(
-                    margin: EdgeInsets.only(right: 30.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        prefixIcon: Icon(
-                          Icons.mail,
-                          size: 28,
-                          color: Color(0xff904c6e),
-                        ),
-                        hintText: "Enter Email",
-                        hintStyle: TextStyle(fontSize: 18.0),
+                    SizedBox(height: 100.0),
+                    Text(
+                      "Name",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
-                  ),
-                  SizedBox(height: 20.0),
-                  Text(
-                    "Password",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  SizedBox(height: 10.0),
-                  Container(
-                    margin: EdgeInsets.only(right: 30.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        prefixIcon: Icon(
-                          Icons.password,
-                          size: 28,
-                          color: Color(0xff904c6e),
+                    SizedBox(height: 10.0),
+                    Container(
+                      margin: EdgeInsets.only(right: 30.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: Icon(
+                            Icons.person,
+                            size: 28,
+                            color: Color(0xff904c6e),
+                          ),
+                          hintText: "Enter Name",
+                          hintStyle: TextStyle(fontSize: 18.0),
                         ),
-                        hintText: "Enter Password",
-                        hintStyle: TextStyle(fontSize: 18.0),
                       ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 20.0),
+                    Text(
+                      "Email",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: 10.0),
+                    Container(
+                      margin: EdgeInsets.only(right: 30.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: Icon(
+                            Icons.mail,
+                            size: 28,
+                            color: Color(0xff904c6e),
+                          ),
+                          hintText: "Enter Email",
+                          hintStyle: TextStyle(fontSize: 18.0),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Text(
+                      "Password",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: 10.0),
+                    Container(
+                      margin: EdgeInsets.only(right: 30.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: Icon(
+                            Icons.password,
+                            size: 28,
+                            color: Color(0xff904c6e),
+                          ),
+                          hintText: "Enter Password",
+                          hintStyle: TextStyle(fontSize: 18.0),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 40.0),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 40.0, right: 50.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Next",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Container(
+                            height: 80,
+                            width: 80,
+                            decoration: BoxDecoration(
+                              color: Color(0xff904c6e),
+                              borderRadius: BorderRadius.circular(60.0),
+                            ),
+                            child: Icon(
+                              Icons.arrow_forward,
+                              color: Colors.white,
+                              size: 40.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 50.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Already have an account?",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Color(0xff904c6e),
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
