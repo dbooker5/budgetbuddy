496fb1d38e9ac5049993f54aafa91828ab10d07b
dbooker5 <bookerotieno21@gmail.com>
2025-12-26 11:32:30 +0300

    feat: Implement login form UI
    - Add a "Welcome Back!" title with specific text styling.
    - Implement text fields for "Email" and "Password," each with a corresponding label.
    - Style the input fields with white backgrounds, rounded corners, and prefix icons for mail and password.

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -20,6 +20,89 @@ class _LoginState extends State<Login> {
               width: MediaQuery.of(context).size.width,
               fit: BoxFit.cover,
             ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 90.0, right: 50.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "Welcome\nBack!",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 70.0),
+                  Text(
+                    "Email",
+                    style: TextStyle(
+                      color: Color(0xff3f3d65),
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Container(
+                    margin: EdgeInsets.only(right: 30.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.mail,
+                          size: 28,
+                          color: Color(0xff904c6e),
+                        ),
+                        hintText: "Enter Email",
+                        hintStyle: TextStyle(fontSize: 18.0),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(
+                    "Password",
+                    style: TextStyle(
+                      color: Color(0xff3f3d65),
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Container(
+                    margin: EdgeInsets.only(right: 30.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.password,
+                          size: 28,
+                          color: Color(0xff904c6e),
+                        ),
+                        hintText: "Enter Password",
+                        hintStyle: TextStyle(fontSize: 18.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
